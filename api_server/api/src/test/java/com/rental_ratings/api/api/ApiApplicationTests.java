5233acc69e25c5f4d171b456935c256896d1b461
William Hendershot <whendershot1@gmail.com>
2022-06-23 10:19:06 +0900

    Initial api server innit

---
+++ api_server/api/src/test/java/com/rental_ratings/api/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rental_ratings.api.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
